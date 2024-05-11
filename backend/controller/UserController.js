1388b30d8c4f6712adbcbad43b269315f56d5431
Harini Senevirathne <99156018+IT21386022@users.noreply.github.com>
2024-05-11 14:43:05 +0530

    init

--- backend/controller/UserController.js
+++ backend/controller/UserController.js
@@ -1,6 +1,6 @@
 const User = require("../model/UserModel");
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+// const bcrypt = require("bcrypt");
 
 const register = async (req, res) => {
   const { fname, lname, email, password } = req.body;
@@ -13,13 +13,13 @@ const register = async (req, res) => {
         return res.status(400).json({ error: 'Email already exists' });
       } else {
         // If email does not exist, hash the password and create the new user
-        bcrypt.hash(password, 10)
-          .then((hash) => {
-            User.create({ fname, lname, email, password: hash })
-              .then((user) => res.json({ status: 'Success' }))
-              .catch((err) => res.status(500).json({ error: 'Internal server error' }));
-          })
-          .catch((err) => res.status(500).json({ error: 'Internal server error' }));
+        // bcrypt.hash(password, 10)
+        //   .then((hash) => {
+        //     User.create({ fname, lname, email, password: hash })
+        //       .then((user) => res.json({ status: 'Success' }))
+        //       .catch((err) => res.status(500).json({ error: 'Internal server error' }));
+        //   })
+        //   .catch((err) => res.status(500).json({ error: 'Internal server error' }));
       }
     })
     .catch((err) => res.status(500).json({ error: 'Internal server error' }));
@@ -31,19 +31,19 @@ const login = async (req, res) => {
   try {
     const user = await User.findOne({ email: email });
     if (user) {
-      bcrypt.compare(password, user.password, (err, response) => {
-        if (response) {
-          const token = jwt.sign(
-            { email: user.email },
-            "jwt-secret-key",
-            { expiresIn: "1d" }
-          );
-          res.cookie("token", token, { httpOnly: true, secure: true });
-          res.json({ status: "success" });
-        } else {
-          res.status(401).json({ error: "Password is incorrect" });
-        }
-      });
+      // bcrypt.compare(password, user.password, (err, response) => {
+      //   if (response) {
+      //     const token = jwt.sign(
+      //       { email: user.email },
+      //       "jwt-secret-key",
+      //       { expiresIn: "1d" }
+      //     );
+      //     res.cookie("token", token, { httpOnly: true, secure: true });
+      //     res.json({ status: "success"s });
+      //   } else {
+      //     res.status(401).json({ error: "Password is incorrect" });
+      //   }
+      // });
     } else {
       res.status(404).json({ error: "User not found" });
     }
