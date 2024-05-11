d02df020082a63f0331c29747fa3aeb16190f12d
Harini Senevirathne <99156018+IT21386022@users.noreply.github.com>
2024-05-11 15:09:01 +0530

    chansged

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(errorHandler);
 
 app.use(
   cors({
-    origin: ["*"],
+    origin: ["http://localhost:3000"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
